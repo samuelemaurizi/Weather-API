4598dce13bc074a23f819cb51424afb851ca99e3
Samuele Maurizi <samuelemaurizi@gmail.com>
2019-08-02 18:33:17 +0200

    add env port

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
-app.listen(3000, () => console.log('Listening on PORT 3000...'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on PORT ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
